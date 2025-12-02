30475572e2952333c07aaefc0963e31a53d2c617
Anton-Osip <osipchika@gmail.com>
2025-12-02 06:57:28 +0300

    fix: pixel-perfect header component

--- src/shared/ui/icons/ChevronVertical/ChevronVertical.tsx
+++ src/shared/ui/icons/ChevronVertical/ChevronVertical.tsx
@@ -1,30 +1,30 @@
-import type { SVGProps } from 'react';
 import s from './ChevronVertical.module.css';
 
-export type ChevronButtonProps = SVGProps<SVGSVGElement> & {
-  size?: number;
-};
-
-export const ChevronVertical = ({ size = 8, className = '', ...props }: ChevronButtonProps) => {
-  const height = (size * 14) / 8;
-
+export const ChevronVertical = ({ className = '', ...props }) => {
   return (
     <svg
-      width={size}
-      height={height}
+      width='10'
+      height='20'
       viewBox='0 0 8 14'
+      fill='none'
       xmlns='http://www.w3.org/2000/svg'
       className={`${s.chevron} ${className}`}
       {...props}
     >
-      <path
-        d='M1 1l6 6-6 6'
-        fill='none'
-        stroke='currentColor'
-        strokeWidth='1.6'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
+      <g clipPath='url(#clip0_411_271)'>
+        <path
+          d='M1.00001 1L7 6.99995L1 13'
+          stroke='#727E9B'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_411_271'>
+          <rect width='14' height='8' fill='white' transform='matrix(0 -1 1 0 0 14)' />
+        </clipPath>
+      </defs>
     </svg>
   );
 };
